3dba7aaa4bd7c21c274331e06a67e1ce84a55d26
Lucernewhy <60300216+gamma776@users.noreply.github.com>
2026-09-23 11:41:06 -0700

    d

---
+++ NoSunshine.java
@@ -0,0 +1,50 @@
+public class NoSunshine {
+   public static void firstLine()
+   {  
+    System.out.println("Ain't no sunshine when she's gone");
+    }
+    public static void endLine() {
+    System.out.println("Anytime she goes away");
+    }
+    public static void twoLine() {
+    System.out.println("Wonder this time where she's gone");
+    }
+    public static void chorus(){
+    System.out.println("I know, I know, I know, I know, I know");
+    }
+    public static void line(String line) {
+    System.out.println(line);
+    }
+
+  public static void main(String[]args) {
+   firstLine();
+   line("It's not warm when she's away");
+   firstLine();
+   endLine();
+   System.out.println(" ");
+   twoLine();
+   line("Wonder if she's gone to stay");
+   twoLine();
+   line(" ");
+   line("And I know, I know, I know, I know, I know");
+   chorus();
+   chorus();
+   chorus();
+   chorus();
+   line("I know, I know");
+   line(" ");
+   line("Hey, I ought to leave young thing alone");
+   line("But ain't no sunshine when she's gone, whoa-whoa");
+   firstLine();
+   line("Only darkness every day");
+   line(" ");
+   firstLine();
+   line("And this house just ain't no home");
+   line(" ");
+   endLine();
+   endLine();
+   endLine();
+   endLine();
+   }
+   
+ }
